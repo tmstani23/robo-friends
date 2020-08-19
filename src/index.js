c2b486f816f5aa7de1cf228893243b2ba2922654
tmstani23 <tmstani23@gmail.com>
2020-08-19 13:46:37 -0500

    redux 2
    Added reducers and connected them to app component.  Added redux-thunk to listen and handle async actions.  Added constants for different promise states and added types.  Connected getRobots action that fetches the robots to the reducer and view.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider, connect} from 'react-redux';
-import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
 import 'tachyons';
 import App from './containers/App';
 import './styles/App.css';
-import { searchRobotsReducer } from './reducers';
+import { searchRobotsReducer, requestRobotsReducer } from './reducers';
 
+const rootReducer = combineReducers({
+  searchRobotsReducer,
+  requestRobotsReducer
+})
 
-const store = createStore(searchRobotsReducer)
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunkMiddleware),
+))
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider reduxStore = {store}> 
+    <Provider store = {store}> 
       <App  />
     </Provider>
   </React.StrictMode>,
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,48 @@
-import { CHANGE_SEARCH_FIELD } from "../constants"
+import { 
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+ } from "../actions/constants";
 
-const initialState = {
+const initialStateSearch = {
     searchField: ''
 }
+const initialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: ''
+}
 
-export const searchRobotsReducer = (state=initialState, action={}) => {
+export const searchRobotsReducer = (state=initialStateSearch, action={}) => {
     switch(action.type) {
         case CHANGE_SEARCH_FIELD:
         return {
             ...state,
             searchField: action.payload
+        }
+        default:
+            return state
+    }
+}
+
+export const requestRobotsReducer = (state=initialStateRobots, action={}) => {
+    switch(action.type) {
+        case REQUEST_ROBOTS_PENDING:
+        return {
+            ...state,
+            isPending: true
+        }
+        case REQUEST_ROBOTS_SUCCESS:
+        return {
+            ...state,
+            isPending: false,
+            robots: action.payload
+        }
+        case REQUEST_ROBOTS_FAILED:
+        return {
+            ...state,
+            isPending: false
         }
         default:
             return state
